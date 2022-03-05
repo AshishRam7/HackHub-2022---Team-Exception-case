b3d9c72ea38169e5af9ff950b8fc7185e67c5255
MasterKavi-24 <srikavipriyan3@gmail.com>
2022-03-05 23:51:05 +0530

    Register And Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 
 # Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
@@ -14,54 +15,59 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
-        
-        
-        if password1==password2:
-            
+
+        if password1 == password2:
+
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username already taken')
-                return render(request,'sign up.html')
-                
-            
+                print("username present")
+                # return render(request, 'sign up.html')
+                return render(request, 'sign in.html')
+
             elif User.objects.filter(email=email).exists():
-                messages.info(request,'email taken')
-                return render(request,'sign up.html')
-                
-                
-            
+                messages.info(request, 'email taken')
+                print("email present")
+                # return render(request, 'sign up.html')
+                return render(request, 'sign in.html')
+
             else:
-            
-                user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
-                user.save();
+
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
                 print('user_created')
-                messages.info(request,'user created')
-                return render(request,'sign in.html')
-               
+                messages.info(request, 'user created')
+                return render(request, 'sign in.html')
 
         else:
             messages.info("password not matching")
+            print("fgdgdfg")
             return redirect('/')
     else:
-        messages.info(request,'password not matching')
-        return render(request,'sign up.html')
+        messages.info(request, 'password not matching')
+        print("fgdgdfgsafsfsfs")
+        return render(request, 'sign up.html')
 
 
 def login(request):
     if request.method == 'POST':
         username = request.POST['user_name']
-        password1 = request.POST['password1']
+        password = request.POST['password']
 
-        user = authenticate(user_name=username, password=password1)
+        user = authenticate(user_name=username, password=password)
 
         if user is not None:
             login(request, user)
+            print("Loggedin")
             return redirect('agri.html')
 
         else:
-            messages.info(request,'invalid credentials')
-            return render(request,'agri.html')
+            print("Invalid duh")
+            messages.info(request, 'invalid credentials')
+            return render(request, 'login.html')
     else:
-        return render(request,'sign in.html')
+        print("Invalid duh2")
+        return render(request, 'sign in.html')
 
 
 def logout(request):
